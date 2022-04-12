f2ad79d17e3792ec1accdc8f13ff83aeb415dada
patrick_akomolafe <patrickakomolafe@gmail.com>
2022-04-11 22:04:28 -0700

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *My hello. printf task
+ *Return: return 1 after program execution
+ */
+int main(void)
+{
+putchar(" and that piece of art is useful" - Dora Korpar, 2015-10-19,\n");
+return (0);
+}
+
